3a8f8a14046a1a16a70339f4ad42505e5a6fe460
Zehra Kırdağ <161854574+zkrdg0@users.noreply.github.com>
2025-04-25 22:55:38 +0300

    Update hranalysis.py

--- hranalysis.py
+++ hranalysis.py
@@ -25,7 +25,6 @@ for col in df.columns:
 X = df.drop('is_promoted', axis=1)  
 y = df['is_promoted']  
 
-#  Veriyi eğitim ve test setlerine ayırma
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 X_test_df = X_test.copy()
